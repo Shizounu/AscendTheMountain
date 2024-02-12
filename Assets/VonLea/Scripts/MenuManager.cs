95f758d181431c9650f9169f8246f16c9ec919a1
Lea Hoppe <leapaetzoldd@gmail.com>
2024-02-12 16:03:24 +0100

    worked on Main Menu
    - imported 2d pixel UI pack
    - imported pixel art backgrounds (some of them are even animated, but had problems with the resolution so just used the unanimated ones)
    - imported pixel art font (wanted to use the existing ones but I would have to convert them all to TextMeshPro to actually use and test them, so I`m gonna stick with the one I found, but it can obviously be changed again)
    - visualized main menu and options with corresponding slider for audio implementation later
    - added credits
    - switch into map scene via seperate menuManager script since the main menu scene would be still open in the map scene if I`d use the LoadScene script to switch
    - quit scene also works

---
+++ Assets/VonLea/Scripts/MenuManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField, Header("References")] private GameObject[] _views;
+
+    private void Awake()
+    {
+        _views[0].SetActive(true);
+    }
+
+    public void LoadScene(int idx)
+    {
+        SceneManager.LoadScene(idx);
+    }
+
+    /// <summary>
+    /// method to switch between views
+    /// </summary>
+    /// <param name="idx">0-Menu;1-Options;2-Credits</param>
+    public void SwitchView(int idx)
+    {
+        if (idx >= _views.Length) return;
+
+        for (int i = 0; i < _views.Length; i++)
+        {
+            _views[i].SetActive(i == idx);
+        }
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+
+}
